3310880a9629cf355b62ace1a4fd4c247f425524
smdekate <smdekate@gmail.com>
2023-11-06 16:36:48 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";   // treat all the JS code as newer version
+
+// alert("Hello, World")   // we are using node.js not browser
+
+console.log(3 
+    +
+    3)  // worstest practice, code redability should be high
+
+let name = "swapnil"
+let age = 24
+let isLoggedIn = true
+let state;
+let temprature = null
+
+// number => 2 to power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => stand alone value
+// undefined
+// symbol => unique
+
+// object
+
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
