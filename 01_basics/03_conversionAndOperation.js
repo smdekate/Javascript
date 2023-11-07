a2dbd52c7d3f871372e6c983201f8d88c070056b
smdekate <smdekate@gmail.com>
2023-11-07 10:54:37 +0530

    Conversion of data types

---
+++ 01_basics/03_conversionAndOperation.js
@@ -0,0 +1,23 @@
+let score = "true"
+// console.log(typeof score)
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "adsf"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true
+// 0 => false
+// "" => flase
+// "non empty string" => true
+
+let someNumber = 33 + 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
